b33bc0f4abf7475c89f0ccf36dc0936d12df346f
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-01 09:52:04 +0530

    ADDED : Welcome to Binary Search Tree in Data Structure

---
+++ src/main/java/com/bridgelabz/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.binarysearchtree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        //Displaying Welcome Message
+        System.out.println("Welcome To Binary Search Tree in Data Structure");
+    }
+}
